529f896e6c171964a8694aa4f1b57e6a849dd2b3
EstimaSahuma <estimaetilenoglicol@gmail.com>
2021-07-28 09:40:08 +0100

    ffff

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -25,7 +25,6 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     MatProgressSpinnerModule
   ],
   exports: [
-    
     BrowserAnimationsModule,
     MatSliderModule,
     MatFormFieldModule,
